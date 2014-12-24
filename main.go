a392baa8bb214257e1180b07d865a6211df1fb0e
Larry Price <larry.price.dev@gmail.com>
2014-12-24 10:31:49 -0500

    statically serving files

--- main.go
+++ main.go
@@ -4,5 +4,6 @@ import "net/http"
 
 func main() {
     http.Handle("/", http.FileServer(http.Dir("views")))
+    http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("./public/js/"))))
     http.ListenAndServe(":5000", nil)
 }
