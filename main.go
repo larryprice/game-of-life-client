29911b6245deb410eb107b7f8313db8afa6f2a69
Larry Price <larry.price.dev@gmail.com>
2014-12-28 20:20:47 -0500

    correcting port

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
-import "net/http"
+import (
+  "net/http"
+  "os"
+)
 
 func main() {
     http.Handle("/", http.FileServer(http.Dir("views")))
     http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("./public/js/"))))
     http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./public/css/"))))
-    http.ListenAndServe(":5000", nil)
+    http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
